356d51fe01c3585fcdc79e60ef3a066037907124
jatin_vermaa <jatin.verma@omnicard.in>
2025-09-30 17:00:41 +0530

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Eroute-Admin on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatTests/ChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatTests.swift
+//  ChatTests
+//
+//  Created by Eroute-Admin on 30/09/25.
+//
+
+import Testing
+
+struct ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
